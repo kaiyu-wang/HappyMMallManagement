aa885e88d3d1f075fea8d7a125b0223270e7709c
Catherine Wang <cathywky123@gmail.com>
2020-03-26 12:17:34 -0400

    public part

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'font-awesome/css/font-awesome.min.css';
-import './index.css';
-import './index.scss';
+import { BrowserRouter as Router, Switch, Route, Link, Redirect} from 'react-router-dom'
+import Layout from 'component/layout/index.jsx';
+//home
+import Home from 'page/home/index.jsx';
+
+class App extends React.Component{
+    render(){
+        return (
+            <Router>
+                <Layout >
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        <Route path="/product" component={Home}/>
+                        <Route path="/product-category" component={Home}/>
+                   
+                        <Redirect from="*" to ="/" />
+                    </Switch>
+                </Layout>   
+            </Router>
+        )
+    }
+}
+
 
 ReactDOM.render(
-    <div>
-        <i class="fab fa-500px"></i>
-        <h1>Hello, world!</h1>
-    </div>,
+    <App/>,
 document.getElementById('app')
 );
---
+++ src/page/home/index.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Link }     from 'react-router-dom';
+import './index.scss'
+import PageTitle    from 'component/page-title/index.jsx';
+
+class Home extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            userCount       : '-',
+            productCount    : '-',
+            orderCount      : '-'
+        }
+    }
+    componentDidMount(){
+        //this.loadCount();
+    }
+    loadCount(){
+        // _statistic.getHomeCount().then(res => {
+        //     this.setState(res);
+        // }, errMsg => {
+        //     _mm.errorTips(errMsg);
+        // });
+    }
+    render(){
+        return (
+            <div id="page-wrapper">
+                <PageTitle title="首页" />
+                <div className="row">
+                    <div className="col-md-4">
+                        <Link to="/user" className="color-box brown">
+                            <p className="count">{this.state.userCount}</p>
+                            <p className="desc">
+                                <i className="fa fa-user-o"></i>
+                                <span>Total User Count</span>
+                            </p>
+                        </Link>
+                    </div>
+                    <div className="col-md-4">
+                        <Link to="/product" className="color-box green">
+                            <p className="count">{this.state.productCount}</p>
+                            <p className="desc">
+                                <i className="fa fa-list"></i>
+                                <span>Total Product Count</span>
+                            </p>
+                        </Link>
+                    </div>
+                    <div className="col-md-4">
+                        <Link to="/order" className="color-box blue">
+                            <p className="count">{this.state.orderCount}</p>
+                            <p className="desc">
+                                <i className="fa fa-check-square-o"></i>
+                                <span>Total Order Count</span>
+                            </p>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Home;
